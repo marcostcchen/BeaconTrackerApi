8a7b025b9b7f6990edfd17388752811a468c23f6
Marcos Chen <marcostcchen@gmail.com>
2021-03-10 17:45:21 -0300

    Api - Added production connection string

--- Database/DbConnection.cs
+++ Database/DbConnection.cs
@@ -10,6 +10,7 @@ namespace BeaconTrackerApi.Database
         public SqlConnection OpenConnection()
         {
             var builder = new SqlConnectionStringBuilder {DataSource = @"localhost\SQLEXPRESS01", UserID = "user", Password = "user", InitialCatalog = "beacontracker"};
+            // var builder = new SqlConnectionStringBuilder {DataSource = "beacontrackerdb.clmsc6yg9pqz.us-east-2.rds.amazonaws.com,1433", UserID = "admin", Password = "Natalia123", InitialCatalog = "beacontrackerdb"};
             _connection = new SqlConnection(builder.ConnectionString);
             if (_connection.State != ConnectionState.Open) _connection.Open();
             return _connection;
